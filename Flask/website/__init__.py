3cea4709c220eb84f4f16f3c09befc28ef11c4b1
Vishal Krishna Kumar <vishal.kumar@amantyatech.com>
2024-11-20 09:58:33 +0530

    [Creating routes/views]

--- Flask/website/__init__.py
+++ Flask/website/__init__.py
@@ -7,5 +7,14 @@ def create_app():
     app = Flask(__name__) #initialling our app, __name__: represents name of the file / name of the file which ran
     app.config['SECRET_KEY'] = 'dcsjndsnd cdnsin' #to encrypt our application, think of it like a password. It will encrypt/secure cookies and session data related to our website. In production, never ever share this secret key
 
+    #here we will register those blueprints. (import)
+
+    # importing.
+    from .views import views 
+    from .auth import auth
+    # registeration with flask
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
 
--- Flask/website/views.py
+++ Flask/website/views.py
@@ -1,1 +1,17 @@
 # will store main views or url end points or actual functioning front end aspect of website.
+# will store standard routes for our websites, where user can actually go to.
+# Home page / or any place where user can navigate to.
+
+from flask import Blueprint
+
+# we will define that this file is blueprint of our application, which simply means it has a bunch of roots(URLs) inside it.
+
+views = Blueprint('views', __name__) #we have setup blueprint for our flask app.
+
+@views.route('/') #whenever this route (/) is hit, home function will be called.
+def home():
+    return "<h1> Test </h1>"
+
+# now we have defined our blueprints. We now need to register these blueprints inside our .init file.
+
+
