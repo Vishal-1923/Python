31c02be27c462e420073d780a9b8e3b207601c65
Vishal Krishna Kumar <vishal.kumar@amantyatech.com>
2025-03-28 15:34:09 +0530

    chapter2: basics

---
+++ PythonTutorials/Chapter2-basics/basics.py
@@ -0,0 +1,12 @@
+line01 = "********************"
+line02 = "*                  *"
+line03 = "*     Welcome!     *"
+
+print('') #for empty line 
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+print('') #this is fine
+        # print('') #will cause error due to bad indentation --> UNEXPECTED INDENTATION
