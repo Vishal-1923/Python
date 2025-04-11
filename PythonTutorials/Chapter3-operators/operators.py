4f15acba6ac763ff1d454b1d048c61054d76fa41
Vishal Krishna Kumar <vishal.kumar@amantyatech.com>
2025-04-11 14:29:31 +0530

    Completed Chapter3 - operators

---
+++ PythonTutorials/Chapter3-operators/operators.py
@@ -0,0 +1,39 @@
+name = "Vishal"
+print(name)
+
+name = 123
+print(name)
+
+a = 5
+b = 3
+
+print(a / b)
+print(a // b)
+print(round(a/b))
+print(a ** b)
+
+a = "Vishal"
+b = "kumar"
+print(a+ " " + b)
+
+a = 42 
+b = 5
+print(a == b)
+
+b = 42
+print(a == b)
+
+
+
+# if-else
+num = 1 #42 or 1
+print('') #will print an empty line
+
+if(num > 10):
+    print("Right ON!")
+else:
+    print("Not Today!!!")
+
+num = 42
+
+print("Right On !") if (num > 10) else print("Not Today !!! ") #Ternary Statement
